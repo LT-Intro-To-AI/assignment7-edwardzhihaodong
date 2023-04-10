a979e2e3ba2dbe2f79802fa34208e11df42a9f81
Edward Dong <edward.zhihao.dong@gmail.com>
2023-04-10 09:43:28 -0500

    a7 kinda trains weirdly? Idk?

--- a7.py
+++ a7.py
@@ -1,1 +1,16 @@
 from neural import *
+
+XOR = [
+    ([0, 0], [0]),
+    ([0, 1], [1]),
+    ([1, 0], [1]),
+    ([1, 1], [0])
+]
+
+nn = NeuralNet(2, 2, 1)
+
+nn.train(XOR)
+
+print(nn.get_ih_weights())
+print()
+print(nn.get_ho_weights())
