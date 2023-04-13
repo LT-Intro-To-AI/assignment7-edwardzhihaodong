0b3cfc181d3795ed1545209cdd3276e4b756d4d1
Edward Dong <edward.zhihao.dong@gmail.com>
2023-04-13 10:11:15 -0500

    a

--- a7.py
+++ a7.py
@@ -1,4 +1,4 @@
-from neural import *
+from neural import NeuralNet
 
 XOR = [
     ([0, 0], [0]),
@@ -7,9 +7,26 @@ XOR = [
     ([1, 1], [0])
 ]
 
-nn = NeuralNet(2, 2, 1)
+nn = NeuralNet(2, 3, 1)
 
 nn.train(XOR)
+
+print(nn.test_with_expected(XOR))
+print()
+print(nn.get_ih_weights())
+print()
+print(nn.get_ho_weights())
+
+XOR2 = [
+    ([0, 0], [0]),
+    ([0, 1], [1]),
+    ([1, 0], [1]),
+    ([1, 1], [0])
+]
+
+nn2 = NeuralNet(2,8,1)
+
+nn2.train(XOR2)
 
 print(nn.get_ih_weights())
 print()
